c733b869c2bac49344dea3172b953284395e6e82
Rafael-Manna <rafaelmannager@gmail.com>
2026-05-05 17:23:20 -0300

    TESTE

--- server.js
+++ server.js
@@ -1,60 +1,73 @@
 import http from 'http'
 import fs from 'fs'
-import queryString from 'querystring'
-import { URLSearchParams } from 'url'
 
 const PORTA = 3000
 const HOST = 'localhost'
-let cursos = [ ]
 
+// Array que funciona como nosso "Banco de Dados" temporário
+let cursos = []
 
 const server = http.createServer((req, res) => {
-    // const {url, method} = req //    const url = req.url    const method = req.method
-    console.log(req.url, ' - ', req.method)// conferir a requisição - URL e Método
-    if(req.url === '/' && req.method === 'GET'){
-        res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})
-        // res.write()
-        res.end('<h1> Página Inicial </h1>')
-    }else if(req.url === '/cadastro' && req.method === 'GET'){
-         res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})   
+    // Rota: Página Inicial
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
+        res.end('<h1>Sistema Acadêmico</h1><a href="/cadastro">Cadastrar Novo Curso</a> | <a href="/cursos">Ver Cursos</a>')
+    } 
+    
+    // Rota: Formulário de Cadastro
+    else if (req.url === '/cadastro' && req.method === 'GET') {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
         res.end(fs.readFileSync('cadastro.html', 'utf-8'))
-    } else if(req.url === '/cursos' && req.method === 'POST'){
-        res.writeHead(200, {"content-type": 'text/html; charset=utf-8'})
-      
-        let dados = ''
+    } 
+    
+    // Rota: Processamento do Cadastro (POST)
+    else if (req.url === '/cursos' && req.method === 'POST') {
+        let corpo = ''
+        
+        // Coleta os pedaços (chunks) de dados enviados pelo formulário
         req.on('data', chunk => {
-            dados += chunk.toString()
+            corpo += chunk.toString()
         })
+
+        // Após receber tudo, processa e salva
         req.on('end', () => {
-            const dados_req = queryString.parse(dados) 
-            const dados_req1 = new URLSearchParams(dados)
-            cursos.push(dados_req)
-            res.end(JSON.stringify(dados_req) + '<br><br>' + JSON.stringify(Object.fromEntries(dados_req1)))
+            const params = new URLSearchParams(corpo)
+            const novoCurso = {
+                curso: params.get('curso'),
+                ch: params.get('ch'),
+                tipo: params.get('tipo')
+            }
+            
+            cursos.push(novoCurso) // Adiciona ao array[cite: 1]
+            
+            // Redireciona ou confirma o sucesso
+            res.writeHead(201, { "Content-Type": "text/html; charset=utf-8" })
+            res.end('<h2>Curso salvo com sucesso!</h2><a href="/cursos">Ver Lista de Cursos</a>')
+        })
+    } 
+    
+    // Rota: Listagem de Cursos (GET)[cite: 1]
+    else if (req.url === '/cursos' && req.method === 'GET') {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" })
+        
+        let listaHtml = '<h1>Cursos Cadastrados</h1><table border="1"><tr><th>Nome</th><th>Carga Horária</th><th>Tipo</th></tr>'
+        
+        // Gera as linhas da tabela dinamicamente[cite: 1]
+        cursos.forEach(c => {
+            listaHtml += `<tr><td>${c.curso}</td><td>${c.ch}</td><td>${c.tipo}</td></tr>`
         })
         
-
-    }
-     else if(req.url === '/cursos' && req.method === 'GET'){
-        res.writeHead(200, {"content-type": 'application/json; charset=utf-8'})
-        res.end(` 
-            <h1> Cursos Cadastrados </h1>
-            <h3> ${cursos.map((curso) => 
-                `<p>Curso: ${curso.curso}</p><p>
-                Carga Horária: ${curso.ch}</p><p>
-                Tipo: ${curso.tipo}</p>`).join('')} </h3>
-            
-            `)
-    }
-    else{
-        res.writeHead(404, {"content-type": 'text/html; charset=utf-8'})
-        res.end('<h1> Página não localizada! </h1>')
-    }
-
-
+        listaHtml += '</table><br><a href="/cadastro">Cadastrar Outro</a>'
+        res.end(listaHtml)
+    } 
     
-
+    // Rota 404
+    else {
+        res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" })
+        res.end('<h1>404 - Not Found</h1>')
+    }
 })
 
-server.listen(PORTA, HOST,  () => {
-    console.log(`Servidor rodando: http://${HOST}:${PORTA}`)
+server.listen(PORTA, HOST, () => {
+    console.log(`Servidor rodando em http://${HOST}:${PORTA}`)
 })
